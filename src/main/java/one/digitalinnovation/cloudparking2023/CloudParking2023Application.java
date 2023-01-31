197d722c97664d129b04a45952cfb982dba3db28
Gui <oxemtex@gmail.com>
2023-01-31 16:25:00 -0300

    Estrutura inicial

---
+++ src/main/java/one/digitalinnovation/cloudparking2023/CloudParking2023Application.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.cloudparking2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParking2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParking2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/cloudparking2023/CloudParking2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.cloudparking2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParking2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
